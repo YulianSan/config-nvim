4bf2ec62f42e403199bd2c82a0988a34cb3d9b0d
YulianSan <knoob0733@gmail.com>
2023-11-04 00:07:01 -0300

    adding tsx to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ require('nvim-treesitter.configs').setup {
   ensure_installed = { 
     'go', 'lua', 'python', 'rust', 'typescript', 'regex', 
     'bash', 'markdown', 'markdown_inline', 'kdl', 'sql', 'org',
-    'javascript', 'html', 'c_sharp', 'php'
+    'javascript', 'html', 'c_sharp', 'php', 'tsx'
   },
 
   highlight = { enable = true },
