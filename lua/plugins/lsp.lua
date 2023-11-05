4951a963c0373dce8f9872780ec9f4cd6c799b6e
YulianSan <knoob0733@gmail.com>
2023-11-04 21:05:44 -0300

    feat: adding files .tsx to auto format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -170,7 +170,7 @@ vim.api.nvim_create_autocmd('FileType', {
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.php", "*.js", "*.html", "*.ts", "*.cs" },
+  pattern = { "*.php", "*.js", "*.html", "*.ts", "*.cs", "*.tsx" },
   callback = function()
     vim.lsp.buf.format { async = false }
   end,
