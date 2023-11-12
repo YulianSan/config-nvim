f2aec769eb1d1011d91b70ceb2f724e241b6bef5
YulianSan <knoob0733@gmail.com>
2023-11-12 19:22:59 -0300

    adding keymaps useful

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,12 +1,19 @@
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
--- twilight
--- buffers
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.api.nvim_set_keymap("n", "tk", ":bnext<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tj", ":bprev<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "th", ":bfirst<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "tl", ":blast<enter>", {noremap=false})
 vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
--- files
+
 vim.api.nvim_set_keymap("n", "<space>q", ":q<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<space>w", ":w<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
@@ -16,6 +23,7 @@ vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", {noremap=true})
 vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", {noremap=true})
+
 vim.keymap.set('n', "<C-p>", "<C-i>", {noremap=true})
 vim.keymap.set('n', "<Tab>", ">>", { noremap = true, silent = true })
 vim.keymap.set('n', "<S-Tab>", "<<", { noremap = true, silent = true })
