4804fc319133c2332a6c611a174bd36efe04d191
YulianSan <knoob0733@gmail.com>
2023-11-15 13:31:12 -0300

    change dashboard and removing indent blank in dashboard

--- lua/options.lua
+++ lua/options.lua
@@ -44,4 +44,5 @@ vim.opt.shiftwidth = 4
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.indent_blankline_filetype_exclude = {'dashboard'}
 
