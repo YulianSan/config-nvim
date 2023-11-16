2227cf03f6bbaed8de8f39c1723ff12220de2fb0
YulianSan <knoob0733@gmail.com>
2023-11-16 14:58:18 -0300

    adding transparency in noice

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -166,6 +166,15 @@ require('lazy').setup({
     lazy = false,
     priority = 1000,
     opts = {},
+    config = function()
+      require("tokyonight").setup({
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      })
+    end
   },
   {
     "windwp/nvim-autopairs",
