8ec3d9d7338cea1de6a0c58d0dc57bef9b94673c
YulianSan <knoob0733@gmail.com>
2023-12-26 19:37:03 -0300

    removing action in visual mode

--- lua/plugins/actions-preview.lua
+++ lua/plugins/actions-preview.lua
@@ -2,4 +2,4 @@ require('actions-preview').setup {
 
 }
 
-vim.keymap.set({ 'n', 'v' }, '<leader>la', require('actions-preview').code_actions)
+vim.keymap.set('n', '<leader>la', require('actions-preview').code_actions)
