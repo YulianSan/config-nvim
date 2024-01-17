42464e700c93b2537f3a478d5e90ecec1a68e7b0
YulianSan <knoob0733@gmail.com>
2024-01-17 17:56:54 -0300

    set tabstop to 2

--- lua/options.lua
+++ lua/options.lua
@@ -33,8 +33,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- Concealer for Neorg
 vim.o.conceallevel=2
 vim.opt.colorcolumn = "80"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 -- vim.cmd(
 --   [[autocmd ColorScheme * highlight ColorColumn guibg=#15161f ctermbg=235]]
 -- )
