36100e61d1b1031ce5424b4a91643ec8f7bd0a71
YulianSan <knoob0733@gmail.com>
2024-02-17 20:34:47 -0300

    updating packages lazy

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -228,7 +228,7 @@ require('lazy').setup({
 
   'navarasu/onedark.nvim', -- Theme inspired by Atom
   'nvim-lualine/lualine.nvim', -- Fancier statusline
-  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines 
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   -- Fuzzy Finder (files, lsp, etc)
